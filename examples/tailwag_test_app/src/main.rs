56b65923326dbe4ef4aef0f67e475d62efbee0a7
Nik <nwakg@pm.me>
2023-09-08 19:11:11 -0700

    Add example / test app

---
+++ examples/tailwag_test_app/src/main.rs
@@ -0,0 +1,31 @@
+use tailwag::{
+    macros::derive_magic,
+    orm::{
+        data_manager::GetTableDefinition,
+        database_definition::database_definition::DatabaseDefinition,
+    },
+    web::application::WebServiceApplication,
+};
+
+derive_magic! {
+pub struct Item {
+    id: uuid::Uuid,
+    name: String,
+    description: Option<String>,
+}}
+
+#[tokio::main]
+async fn main() {
+    // TODO: Goal to make functions / logic easy to override
+    let data_model: DatabaseDefinition = DatabaseDefinition::new_unchecked("postgres")
+        .table(Item::get_table_definition())
+        .into();
+
+    let app: WebServiceApplication = data_model.into();
+
+    app.run().await;
+
+    // DataModelRestServiceDefinition::default()
+    // .with_resource::<Item>("/item")
+    // .run_app();
+}
