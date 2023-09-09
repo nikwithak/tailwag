21d5488aaca77b08207e026afd5d94f1d9b9e4eb
Nik <nwakg@pm.me>
2023-09-08 18:30:33 -0700

    Housekeeping

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-// #[cfg(macros)]
+#[cfg(feature = "deployer")]
+pub use tailwag_deployer as deployer;
+#[cfg(feature = "macros")]
 pub use tailwag_macros as macros;
-
+#[cfg(feature = "orm")]
 pub use tailwag_orm as orm;
+#[cfg(feature = "utils")]
+pub use tailwag_utils as utils;
+#[cfg(feature = "web_service")]
+pub use tailwag_web_service as web;
