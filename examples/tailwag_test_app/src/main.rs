f760d43072ab1e06d992a20d6e1266d6aff451ea
Nik <65584296+nikwithak@users.noreply.github.com>
2023-10-18 14:21:14 -0700

    Housekeeping: Clean up compiler warnings, sync with subrepos

--- examples/tailwag_test_app/src/main.rs
+++ examples/tailwag_test_app/src/main.rs
@@ -1,9 +1,6 @@
 use sqlx::postgres::PgPoolOptions;
 use tailwag::{
-    orm::{
-        data_definition::database_definition::DatabaseDefinition,
-        data_manager::{GetTableDefinition, PostgresDataProvider},
-    },
+    orm::data_manager::{GetTableDefinition, PostgresDataProvider},
     web::{application::WebServiceApplication, traits::rest_api::BuildRoutes},
 };
 
@@ -15,6 +12,8 @@ use tailwag::{
     tailwag::macros::Queryable,
     tailwag::macros::GetTableDefinition,
     tailwag::macros::Insertable,
+    tailwag::macros::Deleteable,
+    tailwag::macros::Updateable,
     tailwag::macros::BuildRoutes,
 )]
 pub struct Item {
@@ -25,11 +24,6 @@ pub struct Item {
 
 #[tokio::main]
 async fn main() {
-    // TODO: Goal to make functions / logic easy to override
-    let data_model: DatabaseDefinition = DatabaseDefinition::new_unchecked("postgres")
-        .table(Item::get_table_definition())
-        .into();
-
     /////////////////////////////////////
     // Boilerplate for DB connectivity //
     /////////////////////////////////////
@@ -50,83 +44,4 @@ async fn main() {
 
     app.run().await;
     todo!()
-
-    // DataModelRestServiceDefinition::default()
-    // .with_resource::<Item>("/item")
-    // .run_app();
 }
-
-// #[async_trait]
-// impl BuildRoutes<Self> for Item {
-//     type Resource = Self;
-
-//     async fn build_routes(data_manager: PostgresDataProvider<Self>) -> Router {
-//         let _ = data_manager;
-//         #[derive(Deserialize)]
-//         pub struct ItemCreateRequest {
-//             name: String,
-//             description: Option<String>,
-//         }
-//         impl Into<Item> for ItemCreateRequest {
-//             fn into(self) -> Item {
-//                 Item {
-//                     id: Uuid::new_v4(),
-//                     name: self.name,
-//                     description: self.description,
-//                 }
-//             }
-//         }
-
-//         // TODO: macro for the function to wrap in `Query` or `Json` automatically, so it can just be a decoration on a logic function.
-//         pub async fn post_item(
-//             State(data_manager): State<PostgresDataProvider<Item>>,
-//             axum::extract::Json(request): axum::extract::Json<ItemCreateRequest>,
-//         ) -> Json<Item> {
-//             let item: Item = request.into();
-//             data_manager.create(&item).await.expect("Unable to create object");
-//             Json(item)
-//         }
-
-//         pub async fn get_items(
-//             State(data_manager): State<PostgresDataProvider<Item>>
-//         ) -> Json<Vec<Item>> {
-//             Json(data_manager.all().execute().await.unwrap())
-//         }
-
-//         data_manager.run_migrations().await.expect("Failed to run migrations");
-// Router::new()
-// .route("/", post(post_item))
-// .route("/", get(get_items))
-//             .with_state(data_manager)
-//     }
-// }
-
-// // TODO: macro for the function to wrap in `Query` or `Json` automatically, so it can just be a macro over a logic function.
-// pub async fn get_food_trucks(
-//     State(data_manager): State<FoodTruckDataManager>,
-//     // Query(request): Query<GetFoodTrucksRequest>,
-// ) -> Json<Vec<FoodTruck>> {
-//     let q = data_manager.all();
-//     let results = q.execute().await.unwrap();
-//     Json(results)
-// // }
-
-// pub async fn form() -> Html<String> {
-//     Html(
-//         "
-//         <form method=\"POST\" encType=\"application/json\" >
-//             <label for=\"name\">Name</label>
-//             <input type=\"text\" name=\"name\" />
-//             <br />
-//             <label for=\"style\">Style</label>
-//             <input type=\"text\" name=\"style\" />
-//             <input type=\"checkbox\" name=\"is_open_late\" value=\"true\" />
-//             <br />
-//             <button formaction=\"/food_truck\" type=\"submit\">Submit</button>
-//         </form>
-//         "
-//         .into(),
-//     )
-// }
-//         todo!()
-//     }
