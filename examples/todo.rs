cc94f3927584ed56f4a3536e98cf1f06bbb8a7d6
Nik <65584296+nikwithak@users.noreply.github.com>
2024-06-13 15:59:28 -0700

    Add TODO example

---
+++ examples/todo.rs
@@ -0,0 +1,35 @@
+use tailwag::macros::derive_magic;
+use tailwag::web::application::WebService;
+
+#[tokio::main]
+async fn main() {
+    #[derive(
+        Clone,
+        Debug,
+        Default,
+        serde::Deserialize,
+        serde::Serialize,
+        sqlx::FromRow,
+        tailwag::macros::GetTableDefinition,
+        tailwag::macros::Insertable,
+        tailwag::macros::Updateable,
+        tailwag::macros::Deleteable,
+        tailwag::macros::Filterable,
+        tailwag::macros::BuildRoutes,
+        tailwag::macros::Id,
+        tailwag::macros::Display,
+        tailwag::forms::macros::GetForm,
+    )]
+    struct Todo {
+        id: uuid::Uuid,
+        title: String,
+        description: String,
+        due_date: chrono::NaiveDateTime,
+    }
+    WebService::builder("Todo Service")
+        .with_resource::<Todo>()
+        .build_service()
+        .run()
+        .await
+        .expect("Web service crashed.");
+}
